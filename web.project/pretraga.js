35801f973fdba01701e83c1c69707133cb813e65
recmerva <reccmerva@gmail.com>
2023-12-21 17:30:42 +0100

    nesto

--- web.project/pretraga.js
+++ web.project/pretraga.js
@@ -1,6 +1,44 @@
 async function searchWeather(){
-	const cityInput = document.getElementById
+	const cityInput = document.getElementById("cityInput").value
+	const widgetContainer = document.getElementById("weather-widget")
 
+	if(cityInput.trim()===""){
+	widgetContainer.innerHTML = ""
+return
+}
+
+try {
 	const apiKey = "ed8a951092ca4eac926183701231912"
+	const apiUrl=`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityinput}&api=no`
+	const response=await fetch (apiUrl)
+
+
+	if(response.ok){
+		const weatherData=await response.json()
+
+		const city=weatherData.location.name
+		const temperature=weatherData.current.temp_c
+		const humidity=weatherData.current.humidity
+
+		widgetContainer.innerHTML=`
+		<div class="weather-info">
+		<h2>Weather in ${city}</h2>
+		<p>Temperature: ${temperature}C</p>
+		<p>Humidity: ${humidity}%</p>
+		<!--Add more data as needed-->
+		</div>
+		`
+		;
 	
+}
+else{
+	alter("failed to fetch weather data")
+}
+	
+	
+}
+catch (error){
+	console.error("An error occurred while fetching weather data:", error);
+	alter("An error occurred. Please try again later")
+}
 }
