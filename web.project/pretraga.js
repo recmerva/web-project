f1b053342452123db03210c75abd4409bc20ae31
recmerva <reccmerva@gmail.com>
2023-12-27 18:50:00 +0100

    nesto

--- web.project/pretraga.js
+++ web.project/pretraga.js
@@ -9,26 +9,29 @@ return
 
 try {
 	const apiKey = "ed8a951092ca4eac926183701231912"
-	const apiUrl=`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityInput}&api=no`
-	const response=await fetch (apiUrl)
+	const apiUrl=`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityInput}&aqi=no`
+	const response=await fetch (apiUrl);
+
 
 
 	if(response.ok){
-		const weatherData=await response.json()
 
-		const city=weatherData.location.name
-		const temperature=weatherData.current.temp_c
-		const humidity=weatherData.current.humidity
+		const weatherData=await response.json();
+		
 
-		widgetContainer.innerHTML=`
-		<div class="weather-info">
-		<h2>Vremenska prognoza u ${city}</h2>
-		<p>Temperatura: ${temperature}°C</p>
-		<p>Vlaznost: ${humidity}%</p>
-		<!--Add more data as needed-->
-		</div>
-		`
-		;
+		 const city=weatherData.location.name
+		 const temperature=weatherData.current.temp_c
+		 const humidity=weatherData.current.humidity
+
+		 widgetContainer.innerHTML=`
+		 <div class="weather-info">
+		 <h2>Vremenska prognoza u ${city}</h2>
+		 <p>Temperatura: ${temperature}°C</p>
+		 <p>Vlaznost: ${humidity}%</p>
+		 <!--Add more data as needed-->
+		 </div>
+		 `
+		 ;
 	
 }
 else{
